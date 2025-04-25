5a25cd9c595fc59cdbb769578ffc432f4252dd4f
昭诗雨 <149286621+zhaoshiyu101107@users.noreply.github.com>
2025-04-26 06:21:08 +0800

    Add files via upload
    Signed-off-by: 昭诗雨 <149286621+zhaoshiyu101107@users.noreply.github.com>

---
+++ 本体/4.0.bat
@@ -0,0 +1,2 @@
+curl -# -O https://autopatchhk.yuanshen.com/client_app/download/pc_zip/20230804185804_eTmE8EZjJZdAJapq/GenshinImpact_4.0.0.zip
+curl -# -O https://autopatchhk.yuanshen.com/client_app/download/pc_zip/20230804185804_eTmE8EZjJZdAJapq/Audio_Chinese_4.0.0.zip
